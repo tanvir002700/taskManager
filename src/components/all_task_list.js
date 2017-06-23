a52e4ffd50789fce83a1bd6a38873cdce533ed12
tanvir002700 <tanvir002700@gmail.com>
2017-06-24 01:55:08 +0600

    update alltask list design

--- src/components/all_task_list.js
+++ src/components/all_task_list.js
@@ -1,20 +1,33 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Table } from 'react-bootstrap';
 
 class AllTaskList extends Component {
     renderTasks() {
         const {tasks} = this.props.tasks;
         console.log('task to show', tasks);
         return (
-            <ul>
-                {
-                    tasks.map(task => {
-                        return(
-                            <li key={task.id}>{task.task}</li>
-                        );
-                    })
-                }
-            </ul>
+            <Table responsive>
+                <caption>Basic Table Layout</caption>
+                <thead>
+                    <tr>
+                        <th>Task</th>
+                        <th>Task Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        tasks.map(task => {
+                            return(
+                                <tr>
+                                    <td key={task.id}>{task.task}</td>
+                                    <td key={task.id+1}> Not implemented yet </td>
+                                </tr>
+                            );
+                        })
+                    }
+                </tbody>
+            </Table>
         );
     }
     render() {
