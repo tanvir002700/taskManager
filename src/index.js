bfb60e8719e2ce0110b681b42b28185edf7b1aa7
tanvir002700 <tanvir002700@gmail.com>
2017-06-23 23:55:48 +0600

    add bootstrap

--- src/index.js
+++ src/index.js
@@ -2,5 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
 import './index.css';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import allRedures from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(allRedures);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root')
+);
