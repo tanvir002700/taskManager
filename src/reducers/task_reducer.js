ce9e67844cb2cce8137bab70187d0c3b2b0b9de2
tanvir002700 <tanvir002700@gmail.com>
2017-06-25 21:53:35 +0600

    add sfcookie module

--- src/reducers/task_reducer.js
+++ src/reducers/task_reducer.js
@@ -1,4 +1,5 @@
 import { ADD_TASK, DELETE_TASK, COMPLETE_TASK, ACTIVE_TASK } from '../constants';
+import { bake_cookie, read_cookie } from 'sfcookies';
 
 const task = (action) => {
     return {
@@ -34,23 +35,26 @@ const activeTask = (state, action) => {
 }
 
 const tasks = (state = [], action) => {
-    let tasks = null;
+    let tasks = [];
+    state = read_cookie('tasks');
     switch (action.type) {
         case ADD_TASK:
             tasks = [...state, task(action)];
-            return tasks;
+            break;
         case DELETE_TASK:
             tasks = removeTask(state, action);
-            return tasks;
+            break;
         case COMPLETE_TASK:
             tasks = completeTask(state, action);
-            return tasks;
+            break;
         case ACTIVE_TASK:
             tasks = activeTask(state, action);
-            return tasks;
+            break;
         default:
             return state;
     }
+    bake_cookie('tasks', tasks);
+    return tasks;
 };
 
 export default tasks;
