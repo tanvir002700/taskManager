f509fb457f1d3743d891b2ee31bcaacb9cd534f3
tanvir002700 <tanvir002700@gmail.com>
2017-06-30 17:01:57 +0600

    complete reducers add functionality

--- src/modules/tasks/reducer.js
+++ src/modules/tasks/reducer.js
@@ -1,14 +1,15 @@
 import * as t from './action_types';
+import Task from './model';
 
 export default (state = [], action) => {
     switch (action.type) {
         case t.ADD:
-            console.log('Came to this reducers');
-            return [
-                // ...
-            ];
+            console.log('State before update', state);
+            let new_state = null;
+            new_state = [...state, new Task(action.payload)];
+            console.log('State after update', new_state);
+            return new_state;
         default:
             return state;
-        // ...
     }
 };
