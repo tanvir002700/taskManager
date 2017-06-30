807598ade917cfb3201f1d7925d1f6d2d44e814d
tanvir002700 <tanvir002700@gmail.com>
2017-06-30 19:19:42 +0600

    add task to redux

--- src/modules/tasks/components/add_tasks.jsx
+++ src/modules/tasks/components/add_tasks.jsx
@@ -3,15 +3,31 @@ import {connect} from 'react-redux';
 import {add} from '../actions';
 
 class AddTasks extends Component {
-    addTask() {
-        console.log(this.props);
-        this.props.add('Task');
+    constructor(props) {
+        super(props);
+        this.state = {
+            task: ''
+        }
     }
+
+    addTask() {
+        this.props.add(this.state.task);
+    }
+
     render() {
         return(
             <div>
-                <h2>Render from Add Task</h2>
-                <button type="button" onClick={() => this.addTask()}>Add</button>
+                <h2>Add Task to Your List</h2>
+                <div className="form-inline">
+                    <div className="form-group">
+                        <input
+                            type="text"
+                            className="form-control"
+                            onChange={event => this.setState({task: event.target.value})}
+                        />
+                        <button type="button" onClick={() => this.addTask()} className="form-control">Add</button>
+                    </div>
+                </div>
             </div>
         );
     }
