2eba47f908357e34022de736676af730b3a72957
tanvir002700 <tanvir002700@gmail.com>
2017-06-30 17:02:30 +0600

    update all task lists

--- src/modules/tasks/components/all_tasks_list.jsx
+++ src/modules/tasks/components/all_tasks_list.jsx
@@ -1,11 +1,24 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { getAll, getActive, getCompleted } from '../selectors';
 
 class AllTasksList extends Component {
     render() {
+        console.log(this.props);
+        // console.log(tasks);
         return(
             <div>Render from All Task List</div>
         );
     }
 }
 
-export default AllTasksList;
+function mapStateToProps() {
+    return createStructuredSelector({
+        tasks: getAll,
+        active: getActive,
+        complete: getCompleted
+    })
+}
+export default connect(
+    mapStateToProps, null)(AllTasksList);
