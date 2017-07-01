db78a5a7a21c074b5b1dd273a2a90ae7a5778a17
tanvir002700 <tanvir002700@gmail.com>
2017-07-01 19:27:55 +0600

    remove used code

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -6,18 +6,6 @@ import '../assets/stylesheets/app.css';
 
 const {TaskCanvas} = tasks.components;
 
-const styles = {
-    headline: {
-        fontSize: 24,
-        paddingTop: 16,
-        marginBottom: 12,
-        fontWeight: 400,
-    },
-    slide: {
-        padding: 10,
-    },
-};
-
 class App extends Component {
     constructor(props) {
         super(props);
